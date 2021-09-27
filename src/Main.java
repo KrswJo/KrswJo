9abd60f75e02bce67fab5930980c7547b4d3193e
KorswJo <kjsw12@naver.com>
2021-09-27 17:47:04 +0900

    버전1

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
+
+// Terminal == 윈도우에서 쓰는 CMD
+
+// git 명령어 인자
+// git ==> 프로그램 (Git 이라는 프로그램을 킬꺼야)
+
+// 명령어 ==>  해당 명령어를 해줘(push, pull, fetch, add, status)
+// 명령어에 대한 예시) 시리야(git) 노래틀어(명령어)
+
+// 인자 ==> 나는 명령어를 쓸 껀데 구체적으로 어떻게 쓸꺼야! ,
+// 인자에 대한 예시) 햄버거집에 가서 얼음없는(인자) 콜라 주문할께요(명령어)
+
+// 맨 처음에 깃을 사용할 때는 git init
+// init은 초기화, 처음 맨들꺼에요!
+// IntelliJ VCS -> Create git Repository
+
+// 버전관리를 하고 싶은 놈을 추가할 때는 git add 파일명이나 폴더명
+// 이 놈 내가 관리할꺼에요.
+// Git -> commit -> 버전관리할 파일들을 추가!
+
+// 버전을 만들 때는 git commit -m "내용"
+// 버전 만든다잉!
+// Git -> commit -> 버전관리할 파일들을 추가! -> Commit Message 내용을 입력 후 -> Commit 누르기
